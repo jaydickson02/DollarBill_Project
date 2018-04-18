702be641555e4484f22a05e9ba17de2b2c6a6662
Jay Dickson <jaydickson02@gmail.com>
2018-04-18 19:07:09 +1000

    Fixed the CSS problem!

--- ExpServer.js
+++ ExpServer.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
 app.set('view engine','pug');
+app.use(express.static('public'));
 
 app.get('/user/:username', function(req,res){
   var username = req.params.username;
